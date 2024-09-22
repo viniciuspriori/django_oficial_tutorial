d0fcbbb29ef896d063615067abdda549f88f3c3f
Vinicius Priori <priori987@gmail.com>
2024-09-22 11:52:36 -0300

    added tests part 1

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,17 +5,16 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-
     def __str__(self):
         return self.question_text       
-    def was_published_recently(self): #função customizada pra saber se a propriedade satisfaz alguma condição da lógica de negócio
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
     def __str__(self): #mostrar string do objeto 
         return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
+from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
-from django.template import loader
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.db.models import F
+from .models import Choice, Question
+from django.views import generic
+from django.utils import timezone
+
 # def index(request):
 #   return HttpResponse("Hello, world. You're at the polls index.")
 
@@ -18,10 +19,9 @@ from django.db.models import F
 #     return HttpResponse(template.render(context, request))
 #     # return HttpResponse(output)
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
 
 # def detail(request, question_id):
 #     try:
@@ -30,15 +30,18 @@ def index(request):
 #         raise Http404("Question does not exist")
 #     return render(request, "polls/detail.html", {"question": question})
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
 # def vote(request, question_id):
 #     return HttpResponse("You're voting on question %s." % question_id)  
@@ -46,7 +49,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])   # type: ignore
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
@@ -63,4 +66,23 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))  # type: ignore
+    
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        # return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html" 
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
