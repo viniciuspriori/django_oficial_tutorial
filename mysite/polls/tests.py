e1318f1e1cca407efe20a578086f0392e8d819b7
Vinicius Priori <priori987@gmail.com>
2024-09-22 23:43:40 -0300

    added more tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -42,7 +42,7 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        self.assertIs(recent_question.was_published_recently(), True)   
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -100,3 +100,25 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question2, question1],
         )
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,)) # type: ignore
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,)) # type: ignore
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
